7967adf3c3b0f056b91c9e6a7444ae671a45246e
Ashutosh <djashutosh673@gmail.com>
2026-04-10 20:43:40 +0530

    Added login middleware protection

--- insert.php
+++ insert.php
@@ -1,9 +1,13 @@
 <?php
-$serverName = "cosmosrds.cnoo4wwa6kfo.ap-south-1.rds.amazonaws.com"; // example: cosmosrds.xxxxx.ap-south-1.rds.amazonaws.com
+$serverName = "cosmosrds.cnoo4wwa6kfo.ap-south-1.rds.amazonaws.com";
+
+// Connection options – FIXED SSL ERROR
 $connectionOptions = [
     "Database" => "Cosmos_Stamp",
     "Uid" => "cosmos",
-    "PWD" => "4321aeiou"
+    "PWD" => "1234",
+    "Encrypt" => false,               // Disable SSL (easiest fix)
+    "TrustServerCertificate" => true  // Optional, but safe to keep
 ];
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
@@ -13,21 +17,21 @@ if ($conn === false) {
 }
 
 // Get form data
-$FullName = $_POST['FullName'];
-$PhoneNumber = $_POST['PhoneNumber'];
-$Email = $_POST['Email'];
-$AddressLine = $_POST['AddressLine'];
-$City = $_POST['City'];
-$State = $_POST['State'];
-$Pincode = $_POST['Pincode'];
-$LoanType = $_POST['LoanType'];
-$LoanAmount = $_POST['LoanAmount'];
-$TenureMonths = $_POST['TenureMonths'];
-$EmploymentType = $_POST['EmploymentType'];
-$CompanyName = $_POST['CompanyName'];
-$MonthlyIncome = $_POST['MonthlyIncome'];
-$AadhaarNumber = $_POST['AadhaarNumber'];
-$PANNumber = $_POST['PANNumber'];
+$FullName = $_POST['FullName'] ?? '';
+$PhoneNumber = $_POST['PhoneNumber'] ?? '';
+$Email = $_POST['Email'] ?? '';
+$AddressLine = $_POST['AddressLine'] ?? '';
+$City = $_POST['City'] ?? '';
+$State = $_POST['State'] ?? '';
+$Pincode = $_POST['Pincode'] ?? '';
+$LoanType = $_POST['LoanType'] ?? '';
+$LoanAmount = $_POST['LoanAmount'] ?? '';
+$TenureMonths = $_POST['TenureMonths'] ?? '';
+$EmploymentType = $_POST['EmploymentType'] ?? '';
+$CompanyName = $_POST['CompanyName'] ?? '';
+$MonthlyIncome = $_POST['MonthlyIncome'] ?? '';
+$AadhaarNumber = $_POST['AadhaarNumber'] ?? '';
+$PANNumber = $_POST['PANNumber'] ?? '';
 
 // Insert query
 $sql = "INSERT INTO Inquiries 
@@ -45,6 +49,11 @@ $stmt = sqlsrv_query($conn, $sql, $params);
 if ($stmt) {
     echo "Data inserted successfully!";
 } else {
-    echo "Error inserting data";
+    echo "Error inserting data:<br>";
+    die(print_r(sqlsrv_errors(), true));
 }
+
+// Close connection
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
 ?>
